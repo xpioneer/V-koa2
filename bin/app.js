ef3cac2e8a9997b5496dad78af3061386095b9ad
xpioneer <>
2017-08-30 23:22:05 +0800

    fix some

--- bin/app.js
+++ bin/app.js
@@ -5,8 +5,6 @@ import Koa from 'koa'
 import Qs from 'koa-qs'
 import Favicon from 'koa-favicon'
 import Body from 'koa-better-body'
-// import Compress from 'koa-compress'
-// import Serve from 'koa-static'
 import Logger from 'koa-logger'
 import Views from 'koa-views'
 import Session from "koa-session2"
@@ -18,18 +16,10 @@ import Middlewares from '../middlewares'
 
 const App = new Koa();
 Qs(App, 'extended')
-// App.use(Compress({
-//   filter: function (content_type) {
-//     // console.log(content_type, /text|javascript|image/i.test(content_type))
-//     return true;
-//   },
-//   threshold: 2048,
-//   flush: require('zlib').Z_SYNC_FLUSH
-// }))//改成Nginx服务
+
 App.use( Favicon(process.cwd() + '/public/favicon.ico') )
 App.use( Views(process.cwd() + '/public/views', { map: { html: 'ejs' } }) )//测试页面
-// App.use( Views(process.cwd() + '/views/dist', { map: { html: 'ejs' } }) )
-// App.use( Serve(process.cwd() + '/views/dist') )//改成Nginx服务
+
 App.use(Body())
 
 if(process.env.NODE_ENV == 'development'){
